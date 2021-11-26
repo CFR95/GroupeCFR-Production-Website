67d9461c4fbfb64eb5cadfae154657a493b7a94b
CFR95 <95095886+CFR95@users.noreply.github.com>
2021-11-26 17:44:50 +0100

    Add files via upload

---
+++ js/scrolling_page.js
@@ -0,0 +1,127 @@
+
+
+/* ------------ navBar ------------*/
+
+ScrollReveal().reveal('.header', {
+    delay: 300,
+    duration: 600,
+    opacity: 0,
+    reset: false
+    
+});
+
+
+/* --------------- blog -----------*/
+
+    ScrollReveal().reveal('.all_items', {
+            delay: 200,
+            distance: '100rem',
+            duration: 600,
+            easing: 'cubic-bezier(0.5, 0, 0, 1)',
+            interval: 0,
+            opacity: 1,
+            origin: 'left',
+            reset: false
+            
+  
+    });
+
+    /* -------- qui sommes-nous ----------- */
+
+  
+    ScrollReveal().reveal('.text_groupecfr', {
+        delay: 200,
+        distance: '10rem',
+        duration: 600,
+        origin: 'right',
+        reset: false
+        
+
+});
+ScrollReveal().reveal('.this_is_who_i_am', {
+    delay: 200,
+    distance: '10rem',
+    duration: 600,
+    origin: 'left',
+    reset: false
+    
+
+});
+
+ScrollReveal().reveal('.bullet_point', {
+    delay: 200,
+    distance: '10rem',
+    duration: 600,
+    origin: 'left',
+    reset: false
+    
+
+});
+
+
+ScrollReveal().reveal('.text_objectif', {
+    delay: 200,
+    distance: '0rem',
+    duration: 600,
+    origin: 'right',
+    reset: false
+    
+
+});
+
+ScrollReveal().reveal('.team', {
+    delay: 200,
+    distance: '10rem',
+    duration: 600,
+    origin: 'bottom',
+    reset: false
+    
+
+});
+
+
+    /* -------- acceuil----------- */
+
+    ScrollReveal().reveal('.partners_img_wrapper', {
+        delay: 200,
+        distance: '10rem',
+        duration: 600,
+        origin: 'bottom',
+        reset: false
+        
+
+});
+ScrollReveal().reveal('.icone_thema', {
+    delay: 200,
+    distance: '10rem',
+    duration: 600,
+    origin: 'left',
+    reset: false
+    
+});
+ScrollReveal().reveal('.stat_box', {
+    delay: 200,
+    distance: '0rem',
+    duration: 600,
+    origin: 'right',
+    reset: false
+    
+});
+
+
+ScrollReveal().reveal('.about_us_img', {
+    delay: 200,
+    distance: '10rem',
+    duration: 600,
+    origin: 'left',
+    reset: false
+    
+});
+ScrollReveal().reveal('.about_us_text', {
+    delay: 200,
+    distance: '0rem',
+    duration: 600,
+    origin: 'right',
+    reset: false
+    
+});
